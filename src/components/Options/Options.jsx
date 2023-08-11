0f4ef9a90f0e130e9327091924649e2461328198
Ilydanyxx <danya.chuyko@icloud.com>
2023-08-11 22:09:12 +0300

    up

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import css from './Options.module.css';
 
-import React, { PureComponent } from 'react';
-
 export default class Options extends PureComponent {
   render() {
     return <div>Options</div>;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import React, { PureComponent } from 'react';
-
 export default class Statistics extends PureComponent {
   render() {
     return <div></div>;
