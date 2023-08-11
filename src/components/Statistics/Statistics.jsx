b8d38c25806c513a654e52e5803b043c5ae5e7cb
Ilydanyxx <danya.chuyko@icloud.com>
2023-08-11 22:07:24 +0300

    up

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import css from './Statistics.module.css';
+
 import React, { PureComponent } from 'react';
 
 export default class Statistics extends PureComponent {
