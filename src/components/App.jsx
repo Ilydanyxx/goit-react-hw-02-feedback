53670c3a084025ce8a2756efea1a52b20afd010a
Ilydanyxx <danya.chuyko@icloud.com>
2023-09-15 02:12:42 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import { Container } from '@mui/material';
 import { Component } from 'react';
 import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './Options/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
 
 export class App extends Component {
   state = {
@@ -8,16 +11,45 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
-  countTotalFeedback = () => {
+  totalFeedback = () => {
     const { good, neutral, bad } = this.state;
     return good + neutral + bad;
+  };
+  feedbackOptions = option => {
+    this.setState(firstState => {
+      return {
+        ...firstState,
+        [option]: firstState[option] + 1,
+      };
+    });
+  };
+  percentage = () => {
+    const { good, neutral, bad } = this.state;
+    const a = good + neutral + bad;
+    return (good * 100) / a;
   };
   render() {
     return (
       <Container maxWidth="xl">
-        <p>please leave feedback</p>
-        <button type="button">good</button>
-        <div>`{this.state.good}`</div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            feedback={key => this.feedbackOptions(key)}
+          />
+        </Section>
+        {!this.totalFeedback() ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Section title="Statistics">
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.totalFeedback()}
+              percentage={this.percentage()}
+            />
+          </Section>
+        )}
       </Container>
     );
   }
